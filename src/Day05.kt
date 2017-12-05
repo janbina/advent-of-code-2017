6c148b0a8824b1e821c831cedfd23cf3367f417e
Jan Bína <binajohny@gmail.com>
2017-12-05 12:58:08 +0100

    Day 5 solution

---
+++ src/Day05.kt
@@ -0,0 +1,36 @@
+package day05
+
+import java.io.File
+import kotlin.test.assertEquals
+
+fun main(args: Array<String>) {
+    val input = File("data/day05.txt").readLines().map { it.toInt() }
+
+    assertEquals(374269, part1(input))
+    assertEquals(27720699, part2(input))
+}
+
+fun part1(input: List<Int>): Int {
+    return countSteps(input, { it.inc() })
+}
+
+fun part2(input: List<Int>): Int {
+    return countSteps(input, { if (it >= 3) it.dec() else it.inc() })
+}
+
+fun countSteps(instructions: List<Int>, instructionTransformation: (Int) -> Int): Int {
+    val inst = instructions.toMutableList()
+    var pc = 0
+    var steps = 0
+
+    while (true) {
+        if (pc !in inst.indices) {
+            return steps
+        }
+        inst[pc] = inst[pc].let {
+            pc += it
+            instructionTransformation(it)
+        }
+        steps++
+    }
+}
